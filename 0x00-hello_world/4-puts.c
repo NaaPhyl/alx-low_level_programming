1f63e387a0e08d7180fafdfd150716d09c3816a4
PhylNaa <pnlotdh@gmail.com>
2022-09-10 02:20:30 -0700

    works

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
-* 
-* Description: using the main fuction
-* this program prints "Programming is like building a multilingual puzzle"
 * Return: 0
 */
 int main(void)
