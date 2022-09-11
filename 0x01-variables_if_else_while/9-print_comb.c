e67047f7c1fdab5648aab965200a8eeb597bd442
PhylNaa <pnlotdh@gmail.com>
2022-09-11 08:07:56 -0700

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digit numbers in the formart
+ *
+ * Description: Separated by commas
+ * Return: Always(0) Success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+
+		return (0);
+}
