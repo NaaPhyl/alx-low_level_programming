48a70019e337b8c5bda5489072f93081371be76e
PhylNaa <pnlotdh@gmail.com>
2022-09-11 07:45:35 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 100; p++)
+	{
+		putchar((p / 10) + '0');
+		putchar((p % 10) + '0');
+		if (p != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
