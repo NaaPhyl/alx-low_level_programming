9b6845b80fd598421428e9099d9d5163b2c6d45d
PhylNaa <pnlotdh@gmail.com>
2022-09-19 14:31:56 -0700

    a function that reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,22 @@
-#include " main.h"
-
-/**
- * print_rev prints a string in reverse
- * @s: string to be printed
- * Return: returns nothing
- */
-
-void print_rev(char *s)
-{
-	char rev = s[0];
-	int counter = 0;
-	int i;
-
-	while (s[counter] i = '\n')
+#include "main.h"                                                                                                          
+                                                                                                                        
+/**                                                                                                                         
+ * print_rev prints a string in reverse                                                                                     
+ * @s: string to be printed                                                                                                 
+ * Return: returns nothing                                                                                                  
+ */                                                                                                                         
+                                                                                                                
+void print_rev(char *s)                                                                                                     
+{                                                                                                                           
+        char rev = s[0];                                                                                                    
+        int counter = 0;                                                                                                    
+        int i;                                                                                                              
+                                                                                                             
+        while (s[counter] i = '\n')                                                                                         
 		counter++;
 	for (i = 0; i < counter; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-	}
+		counter--;                                                                                                  
+		rev = s[i];                                                                                                 
+		s[i] = s[counter];                                                                                          	    }                                                                                                                   
 }
