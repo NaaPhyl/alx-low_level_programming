67a019299a7bc941c7348021470a91157c9bab24
PhylNaa <pnlotdh@gmail.com>
2022-09-20 06:34:46 -0700

    a function that prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
- * @s: input string.
+ * puts_half - prints half of a string.
+ * @str: input string.
  * Return: no return.
  */
-void print_rev(char *s)
+void puts_half(char *str)
 {
-	int count = 0;
+	int count = 0, i;
 
 	while (count >= 0)
 	{
-		if (s[count] == '\0')
+		if (str[count] == '\0')
 			break;
 		count++;
 	}
 
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
