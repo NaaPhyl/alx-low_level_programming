10ea79f2d3127ef4d0f06248f50e79d28874f858
PhylNaa <pnlotdh@gmail.com>
2022-09-21 18:55:41 -0700

    A function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,24 @@
 #include "main.h"
 
-
-
 /**
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
 
-*_isupper - checks if a  character is uppercase or not
-
-*@c: character to be tested
-
-*Return: 1 whether it is, 0 otherwise
-
-*/
-
-
-
-int _isupper(int c)
-
+char *_strcat(char *dest, char *src)
 {
 
-	if ((c >= 'A') && (c <= 'Z'))
+	int index = 0, dest_len = 0;
 
-	{
+	while (dest[index++])
+		dest_len++;
 
-		return (1);
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-	}
-
-
-
-	return (0);
-
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,44 +2,44 @@
 
 #define MAIN_H
 
-
+#include <stdio.h>
 
 /**
 
-*main - header file for protoypes
+ *void prototypes()
 
-*void protoypes (void)
+ *int prototypes()
 
-*int protoypes (int)
+ *char prototypes()
 
-*/
+ */
 
-
-
-void print_numbers(void);
-
-void print_most_numbers(void);
-
-void more_numbers(void);
-
-void print_line(int n);
-
-void print_diagonal(int n);
-
-void print_square(int size);
-
-void print_triangle(int size);
+void reverse_array(int *a, int n);
 
 void print_number(int n);
 
-int _putchar(char);
+void print_buffer(char *b, int size);
 
-int _isupper(int);
+int _strcmp(char *s1, char *s2);
 
-int _isdigit(int);
+char *_strcat(char *dest, char *src);
 
-int mul(int, int);
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 
 
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
