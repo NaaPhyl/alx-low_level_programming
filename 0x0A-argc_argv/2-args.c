0e4bb42ba9242d69b27363c80dea50a93b5d79ca
PhylNaa <pnlotdh@gmail.com>
2022-10-02 14:25:03 -0700

    A Write a program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-#include <stdlib.h>
+
 
 /**
 
- * main - program that prints its name, followed by a new line
+ * main - prints all arguments it recieves
 
- * @argc: argument that counts argument input
+ * @argc: number of arguments passed to the function
 
- * @argv: argument that stores the strings in an array of char* (strings)
+ * @argv: argument vector of pointers to strings
 
- * Return: 0
+ *
+
+ * Return: always 0
 
  */
 
@@ -18,25 +20,15 @@ int main(int argc, char *argv[])
 
 {
 
-	int num_1, num_2, mul;
+	int i;
 
 
 
-	if (argc != 3)
-
-		printf("Error\n");
-
-	else
+	for (i = 0; i < argc; i++)
 
 	{
 
-		num_1 = atoi(argv[1]);
-
-		num_2 = atoi(argv[2]);
-
-		mul = num_1 * num_2;
-
-		printf("%d\n", mul);
+		printf("%s\n", argv[i]);
 
 	}
 
