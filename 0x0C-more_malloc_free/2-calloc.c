ee148f9424d8bc86f57d33748a78f4c5ebf96c03
PhylNaa <pnlotdh@gmail.com>
2022-10-07 15:15:09 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,39 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - Concatenates two strings.
- * @s1: The string to be concatenated upon.
- * @s2: The string to be concatenated to s1.
- * Return: If concatenation fails - NULL.
- *         Otherwise - a pointer the newly-allocated space in memory
- *                     containing the concatenated strings.
+ * _calloc - A function that allocates memory for an array using malloc
+ * @nmemb: number of memory spaces
+ * @size: size of nmemb
+ * Return: returns pointer to allocated space, or NULL on failure
  */
 
-char *str_concat(char *s1, char *s2)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	unsigned int i = 0;
+	char *space;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	space = malloc(nmemb * size);
+	if (space == NULL)
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+		return (NULL);
 
-	return (concat_str);
+	for (; i < nmemb * size; i++)
+		*(space + i) = 0;
+
+	return (space);
 }
